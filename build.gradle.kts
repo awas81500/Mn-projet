ec15924143343089859322902a3e860ee07c637a
awa <awa@gmail.com>
2026-04-18 08:31:10 +0000

    fix gradle setup working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -13,7 +14,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
+application {
+    mainClass.set("Execute")
+}
 tasks.test {
     useJUnitPlatform()
 }
